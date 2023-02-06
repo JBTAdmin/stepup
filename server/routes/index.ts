52b405c27477018637703ecc2b75a7465942699b
JBT <ADMIN@JAVABEGINNERSTUTORIAL.COM>
2023-02-06 10:31:57 +0530

    Initial Commit

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-export  const homeRouter = express.Router();
+export const homeRouter = express.Router();
 
 /* GET home page. */
 homeRouter.get('/', function(req, res, next) {
